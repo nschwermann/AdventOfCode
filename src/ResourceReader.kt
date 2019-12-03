43c4a1809fa050d3758b8441a3ebfa6420cd9870
Nathan Schwermann <schwiz@gmail.com>
2019-12-02 21:06:30 -0600

    Days 1 & 2

---
+++ src/ResourceReader.kt
@@ -0,0 +1,8 @@
+class ResourceReader(private val year : Int,
+                     private val day : Int) {
+
+    private fun text() : String = javaClass.getResource("/_$year/Day${day.toString().padStart(2, '0')}").readText()
+    fun ints() = text().lines().map { it.toInt() }
+    fun lines() = text().lines()
+    fun intArray() = text().split(',').map { it.toInt() }.toIntArray()
+}
