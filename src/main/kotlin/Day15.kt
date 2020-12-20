559c8bbbc3e8896862cf9bf1ca3ca43bd02a8bfa
Nathan Schwermann <schwiz@gmail.com>
2020-12-20 15:56:52 -0600

    Day 15  - Rambunctious Recitation
     https://adventofcode.com/2020/day/15

---
+++ src/main/kotlin/Day15.kt
@@ -0,0 +1,29 @@
+/**
+ * --- Rambunctious Recitation ---
+ * https://adventofcode.com/2020/day/15
+ */
+fun main() {
+    val input = listOf(2,1,10,11,0,6)
+    println(solver(input, 2020L))
+    println(solver(input, 30_000_000))
+}
+
+private fun solver(input : List<Int>, spoken : Long) : Long {
+    val track = input.foldIndexed(mutableMapOf<Long, Long>()){index,acc, next ->
+        acc.apply {
+            put(next.toLong(), index + 1L)
+        }
+    }
+    val turns = LongRange(input.size + 1L, spoken)
+    return turns.runningFold(0L){acc, turn ->
+        track.getOrDefault(acc, 0L).let { last ->
+            if(!track.containsKey(acc)){
+                0
+            } else {
+                turn - last
+            }.also {
+                track[acc] = turn
+            }
+        }
+    }.dropLast(1).last()
+}
